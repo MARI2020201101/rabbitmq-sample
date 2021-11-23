3389221deccc357b43f7f268c5bbf52edc72ed51
YOOK SEUNGHYEON <shyuk2@iparking.co.kr>
2021-11-23 10:52:27 +0900

    Practice rabbit-mq

---
+++ rabbit-mq-publisher/src/main/java/com/mari/publisher/PublishController.java
@@ -0,0 +1,22 @@
+package com.mari.publisher;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublishController {
+    private static final String EXCHANGE_NAME = "sample.exchange";
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sample/queue")
+    public String samplePublish() {
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, "sample.mari",
+                "RabbitMQ + Springboot = Success!");
+        return "message sending!";
+    }
+
+}
